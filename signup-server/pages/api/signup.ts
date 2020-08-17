fd230bfa9996e74408aebb704d24e298429bec54
Enzo Ferey <enzo.ferey@chance.co>
2020-08-17 22:25:11 +0200

    Generate Discord token.

--- signup-server/pages/api/signup.ts
+++ signup-server/pages/api/signup.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Airtable from "airtable";
+import axios from "axios";
 
 Airtable.configure({
   apiKey: process.env.AIRTABLE_API_KEY,
@@ -10,6 +11,19 @@ const base = new Airtable.base(process.env.AIRTABLE_BASE_ID);
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await createNewUser(req.body.email);
+    const inviteResponse = await axios.post(
+      `https://discord.com/api/channels/${process.env.INVITE_CHANNEL_ID}/invites`,
+      {
+        max_uses: 1,
+        unique: true,
+      },
+      {
+        headers: {
+          Authorization: `Bot ${process.env.INVITE_BOT_TOKEN}`,
+        },
+      }
+    );
+    const inviteLink = `https://discord.gg/${inviteResponse.data.code}`;
     res.statusCode = 200;
     res.json({ success: true });
   } catch (error) {
