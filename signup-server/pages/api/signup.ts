23f10969cba850a9194b210331ab9467ff15423f
Enzo Ferey <enzo.ferey@chance.co>
2020-08-17 23:27:10 +0200

    Send email.

--- signup-server/pages/api/signup.ts
+++ signup-server/pages/api/signup.ts
@@ -1,29 +1,45 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import Airtable from "airtable";
+import fs from "fs";
+import path from "path";
 import axios from "axios";
+import sendgrid from "@sendgrid/mail";
+import Airtable from "airtable";
+import cheerio from "cheerio";
 
+// Load Signup email data
+const signupEmailPath = path.resolve(
+  "public",
+  "emails",
+  "signup",
+  "signup-email.html"
+);
+const signupEmailHtml = fs.readFileSync(signupEmailPath, "utf8");
+const $ = cheerio.load(signupEmailHtml);
+const signupEmailText = $("html *")
+  .contents()
+  .map(function () {
+    return this.type === "text" ? $(this).text() : "";
+  })
+  .get()
+  .join("\n");
+
+// Setup SendGrid
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+// Setup AirTable
 Airtable.configure({
   apiKey: process.env.AIRTABLE_API_KEY,
 });
-
 const base = new Airtable.base(process.env.AIRTABLE_BASE_ID);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    await createNewUser(req.body.email);
-    const inviteResponse = await axios.post(
-      `https://discord.com/api/channels/${process.env.INVITE_CHANNEL_ID}/invites`,
-      {
-        max_uses: 1,
-        unique: true,
-      },
-      {
-        headers: {
-          Authorization: `Bot ${process.env.INVITE_BOT_TOKEN}`,
-        },
-      }
-    );
-    const inviteLink = `https://discord.gg/${inviteResponse.data.code}`;
+    const email = req.body.email;
+
+    await createNewUser(email);
+    const inviteLink = await generateInviteLink();
+    await sendSignupSuccessEmail(email, inviteLink);
+
     res.statusCode = 200;
     res.json({ success: true });
   } catch (error) {
@@ -40,4 +56,33 @@ function createNewUser(email: string): Promise<void> {
       },
     },
   ]);
+}
+
+async function generateInviteLink(): Promise<string> {
+  const inviteResponse = await axios.post(
+    `https://discord.com/api/channels/${process.env.INVITE_CHANNEL_ID}/invites`,
+    {
+      max_uses: 1,
+      unique: true,
+    },
+    {
+      headers: {
+        Authorization: `Bot ${process.env.INVITE_BOT_TOKEN}`,
+      },
+    }
+  );
+  return `https://discord.gg/${inviteResponse.data.code}`;
+}
+
+async function sendSignupSuccessEmail(
+  email: string,
+  inviteLink: string
+): Promise<void> {
+  await sendgrid.send({
+    to: email,
+    from: process.env.SENDGRID_SENDER_EMAIL,
+    subject: "Welcome to Toptile !",
+    text: signupEmailText.split("DISCORD_INVITE_LINK").join(inviteLink),
+    html: signupEmailHtml.split("DISCORD_INVITE_LINK").join(inviteLink),
+  });
 }
